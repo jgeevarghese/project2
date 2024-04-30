4d864af0c75de43844677ae23c816d6aad193238
jgeevarghese <89812287+jgeevarghese@users.noreply.github.com>
2024-04-30 12:04:41 -0400

    update

--- src/media-image.js
+++ src/media-image.js
@@ -1,5 +1,7 @@
+
 import { LitElement, html, css } from 'lit';
 import { DDD } from "@lrnwebcomponents/d-d-d/d-d-d.js";
+
 
 export class mediaImage extends DDD {
 
@@ -9,49 +11,64 @@ export class mediaImage extends DDD {
 
   constructor() {
     super();
+    this.opened = false;
     this.imageSrc = "";
     this.imageAltText = "";
-    this.addEventListener("click", this.clickEvent);
-  }
-
-  clickEvent(e){
-    this.dispatchEvent(new CustomEvent("openMediaImage"), {
-      detail: "no"
-    });
+    this.description = "";
+    this.addEventListener("click", this.openDialog);
   }
 
   static get styles() {
     return css`
-    img{
-      border: solid 2px black ;
-      border-radius: 8px;
-      cursor: pointer;
-      transition: all ease-in .3s;
-      display: inherit;
-      transition: all .3s ease-in;
-      height: 200px;
-      width: auto;
+    img {
+    border: solid 2px var(--ddd-theme-default-keystoneYellow) ;
+    border-radius: 8px;
+    cursor: pointer;
+    transition: all ease-in .3s;
+    display: inherit;
+    margin: 32px 0;
+    transition: all .3s ease-in;
+    height: 200px;
+    width: auto;
     }
     img:hover {
       transform: translate(8px,-8px);
-      box-shadow: -8px 8px #000
+      box-shadow: -8px 8px var(--ddd-theme-default-keystoneYellow)
     }
+
+
+
     `;
   }
 
-  
+  openDialog(e){
+    const evt = new CustomEvent("media-clicked", {
+      bubbles: true,
+      composed: true,
+      cancelable: true,
+      detail: {
+      opened: true,
+      invokedBy: this.invokedBy,
+      },
+      });
+    this.dispatchEvent(evt);
+  }
+
   render() {
     return html`
     <div class="image">
       <img src="${this.imageSrc}" alt="${this.imageAltText}">
+      <div class="desccription">${this.description}</div>
     </div>
     `;
   }
 
   static get properties() {
     return {
+      opened: {type: Boolean},
       imageSrc: {type: String},
-      imageAltText: {type: String}
+      imageAltText: {type: String},
+      description: {type: String},
     };
   }
 }
--- src/play-list.js
+++ src/play-list.js
@@ -1,7 +1,7 @@
 import { LitElement, html, css } from 'lit';
 import { DDD } from "@lrnwebcomponents/d-d-d/d-d-d.js";
 
-export class playList extends DDD {
+export class PlayList extends DDD {
 
   static get tag() {
     return 'play-list';
@@ -9,36 +9,70 @@ export class playList extends DDD {
 
   constructor() {
     super();
-    this.playListItems = [];
-  
+    this.currentSlideIndex = 0;
+    this.slideArray = [];
+    this.showSlideshow = false;
+    this.slideshowImages = [];
   }
 
   static get styles() {
     return css`
-
+     
     `;
   }
 
+  firstUpdated() {
+    const mediaImages = this.shadowRoot.querySelectorAll("media-image");
+    mediaImages.forEach(img => {
+      this.slideArray.push(img.getAttribute("imageSrc"));
+    });
+  }
+
+  handleImageClick(imageSrc) {
+    this.slideshowImages = [...this.slideArray];
+    this.currentSlideIndex = this.slideshowImages.indexOf(imageSrc);
+    this.showSlideshow = true;
+  }
+
+  closeSlideshow() {
+    this.showSlideshow = false;
+    this.slideshowImages = [];
+  }
+
+  previousSlide() {
+    this.currentSlideIndex = (this.currentSlideIndex - 1 + this.slideshowImages.length) % this.slideshowImages.length;
+  }
+
+  nextSlide() {
+    this.currentSlideIndex = (this.currentSlideIndex + 1) % this.slideshowImages.length;
+  }
 
   render() {
     return html`
-      <dialog class="playList">
+      <dialog class="playList" ?open="${this.showSlideshow}" @click="${this.closeSlideshow}">
         <button class="exitButton">x</button>
         <div>
-          <button class="leftButton"> < </button>
-          <img class="blah" src="https://help.figma.com/hc/article_attachments/20650187664151" alt="Italian Trulli" hieght="300px" width="300px">
-          <button class="rightButton"> > </button>
+          <button @click="${this.previousSlide}"> < </button>
+          <img class="preview" src="${this.slideshowImages[this.currentSlideIndex]}" alt="Slide" height="300px" width="300px">
+          <button @click="${this.nextSlide}"> > </button>
         </div>
       </dialog>
+      <div>
+        ${this.slideArray.map(imageSrc => html`
+          <img class="previewImage" src="${imageSrc}" alt="Preview Image" @click="${() => this.handleImageClick(imageSrc)}">
+        `)}
+      </div>
       `;
   }
 
   static get properties() {
     return {
-      playListItems: {type: Array},
-
+      currentSlideIndex: { type: Number },
+      slideArray: { type: Array },
+      showSlideshow: { type: Boolean },
+      slideshowImages: { type: Array }
     };
   }
 }
 
-globalThis.customElements.define(playList.tag, playList);
+customElements.define(PlayList.tag, PlayList);
